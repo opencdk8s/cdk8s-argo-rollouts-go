402ce05a4cb355520796673cc481a3d18be89994
Hunter-Thompson <aatman@auroville.org.in>
2021-08-13 14:36:42 +0000

    chore(release): v0.0.5

--- opencdk8scdk8sargorollout/jsii/jsii.go
+++ opencdk8scdk8sargorollout/jsii/jsii.go
@@ -14,7 +14,7 @@ import (
 	cdk8s       "github.com/cdk8s-team/cdk8s-go/cdk8s/jsii"
 )
 
-//go:embed opencdk8s-cdk8s-argo-rollout-0.0.6.tgz
+//go:embed opencdk8s-cdk8s-argo-rollout-0.0.5.tgz
 var tarball []byte
 
 // Initialize loads the necessary packages in the @jsii/kernel to support the enclosing module.
@@ -26,5 +26,5 @@ func Initialize() {
 	constructs.Initialize()
 
 	// Load this library into the kernel
-	_jsii_.Load("@opencdk8s/cdk8s-argo-rollout", "0.0.6", tarball)
+	_jsii_.Load("@opencdk8s/cdk8s-argo-rollout", "0.0.5", tarball)
 }
